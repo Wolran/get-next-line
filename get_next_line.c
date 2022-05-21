815568b26af47e58204c9fb07fbabb12b9bc6698
vmuller <38337827+Wolran@users.noreply.github.com>
2022-05-22 01:29:01 +0200

    v0.9

--- get_next_line.c
+++ get_next_line.c
@@ -17,25 +17,27 @@ char	*get_next_line(int fd)
 	char	*buffer;
 	char	*str;
 	int		flag;
+	int		size;
 
 	if (BUFFER_SIZE <= 0)
 		return (NULL);
-	buffer = ft_calloc(((BUFFER_SIZE / BUFFER_SIZE) + 1), sizeof(char));
+	size = BUFFER_SIZE / BUFFER_SIZE;
+	buffer = ft_calloc((size + 1), sizeof(char));
 	str = NULL;
-	flag = read(fd, buffer, (BUFFER_SIZE / BUFFER_SIZE));
+	flag = read(fd, buffer, size);
 	if (flag > 0)
-		str = ft_calloc(((BUFFER_SIZE / BUFFER_SIZE) + 1), sizeof(char));
+		str = ft_calloc((size + 1), sizeof(char));
 	while (flag > 0 && buffer[0] != '\n')
 	{
 		str = ft_strjoin(str, buffer);
-		flag = read(fd, buffer, (BUFFER_SIZE / BUFFER_SIZE));
+		flag = read(fd, buffer, size);
 	}
 	if (buffer[0] == '\n')
 		str = ft_strjoin(str, "\n");
 	free(buffer);
 	return (str);
 }
-
+/*
 # include <stdio.h>
 # include <sys/types.h>
 # include <sys/stat.h>
@@ -49,4 +51,4 @@ int	main(void)
 	for (int i = 0; i < 10; i++)
 		printf("%s", get_next_line(fd));
 }
-
+*/
